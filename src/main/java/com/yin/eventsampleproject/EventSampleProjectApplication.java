9e95b84112f3f2a7f737f6e37e719bbf90503b63
youngyin <57608725+youngyin@users.noreply.github.com>
2024-12-02 23:16:21 +0900

    init project

---
+++ src/main/java/com/yin/eventsampleproject/EventSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yin.eventsampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSampleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSampleProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yin/eventsampleproject/EventSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yin.eventsampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSampleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
